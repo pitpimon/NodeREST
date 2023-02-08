a5a7f70ffd0ab4d30f33722af6c3710b5088d6a3
Anirach <anirach.m@fitm.kmutnb.ac.th>
2023-02-08 11:58:40 +0700

    NoDBendSec01

--- src/index.js
+++ src/index.js
@@ -2,14 +2,12 @@ require("dotenv").config();
 
 const express = require("express");
 const app = express();
-const port =  process.env.PORT||3000;
+const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello World!, I love it");
 });
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
-
-
