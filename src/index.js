eb7b9e62c9fdb93d3bf989e50020888f4dda064d
Anirach <anirach.m@fitm.kmutnb.ac.th>
2023-02-09 11:47:07 +0700

    end SecB

--- src/index.js
+++ src/index.js
@@ -5,10 +5,11 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!, I love it");
+  res.send("Hello World!");
 });
 
 app.listen(port, () => {
+  
   console.log(`Example app listening at http://localhost:${port}`);});
 
 //Hello
