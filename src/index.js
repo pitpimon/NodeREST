dd003f8de6b472fe041fe6dca668ee158330a1a9
Anirach <anirach.m@fitm.kmutnb.ac.th>
2023-02-15 11:47:43 +0700

    save index

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World! I am on cloud now" );
+  res.send("Hello World! I am vary happy" );
 });
 
 app.listen(port, () => {
